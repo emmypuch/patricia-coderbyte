82eef81097dd6f63d85de14b200fc6d1c0eab12c
Omowunmi Adesuyi <adesuyiomowunmi@gmail.com>
2022-03-25 02:04:01 +0100

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form>
+      <label for="address">Ethereum Address</label>
+      <input
+        type="text"
+        id="address"
+        name="address"
+        placeholder="Eth Address"
+        aria-label="Address to send eth"
+      />
+
+      <label for="amount">Enter Amount</label>
+      <input
+        type="number"
+        id="amount"
+        name="amount"
+        :value="`${cryptoAmount}BTC`"
+      />
+      <div>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+const Web3 = require("web3");
+const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+import { CONTRACT_ABI } from "./data/contract-info";
+const contract = new web3.eth.Contract(CONTRACT_ABI);
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  mounted() {
+    console.log(contract);
+  },
+};
 </script>
 
 <style>
@@ -24,5 +47,67 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+form {
+  background-color: teal;
+  padding: 4rem;
+  width: 60%;
+  height: 250px;
+  border-radius: 5px;
+}
+
+label {
+  display: flex;
+  color: rgb(221, 236, 236);
+  font-size: 1.1rem;
+}
+
+label[for="number"] {
+  margin-top: 10px;
+}
+
+input[type="text"],
+input[type="number"] {
+  border: none;
+  border-radius: 5px;
+  width: 95%;
+  outline: none;
+  color: rgb(83, 170, 170);
+  font-size: 1rem;
+  display: inline-block;
+  padding: 12px 20px;
+  margin: 10px 0;
+}
+
+input[type="text"] {
+  margin-bottom: 15px;
+}
+
+input[type="number"] {
+  margin-top: 15px;
+}
+
+::placeholder {
+  color: rgb(83, 170, 170);
+}
+
+button {
+  margin-top: 30px;
+  border: none;
+  border-radius: 5px;
+  font-size: 1rem;
+  text-align: center;
+  padding: 10px 30px;
+  color: teal;
+  cursor: pointer;
+}
+
+button:hover {
+  background: rgb(70, 158, 158);
+  transition: 3s ease-in;
 }
 </style>
